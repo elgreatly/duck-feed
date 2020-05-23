b0800071972d8bb148d61f75a3e735d71633669b
elgreatly <abdo.elgreatly@gmail.com>
2020-05-23 15:47:16 +0200

    change success created message

--- src/infrastructure/enums/error-messages.enum.ts
+++ src/infrastructure/enums/error-messages.enum.ts
@@ -3,7 +3,7 @@ export enum ErrorMessagesEnum {
     invalidTime = 'messages.invalidTime',
     invalidString = 'messages.invalidString',
     invalidNumber = 'messages.invalidNumber',
-    successCreated = 'messages.successCreate',
+    successCreated = 'messages.successCreated',
     invalidBoolean = 'messages.invalidBoolean',
     successCreatedAndScheduled = 'messages.successCreatedAndScheduled',
 }
