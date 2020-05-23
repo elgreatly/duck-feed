73e504fe65a1b4bc7ae5afce3c517fc44899a3f3
elgreatly <abdo.elgreatly@gmail.com>
2020-05-24 01:08:17 +0200

    add unit and integration test

--- src/app/duck-feed/services/duck-feed.service.ts
+++ src/app/duck-feed/services/duck-feed.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { AddDuckFeedRequestDto } from '../dto/request/add-duck-feed-request.dto';
 import { DuckFeedRepository } from '../repository/duck-feed.repository';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { DuckFeedMoel } from '../models/duck-feed.model';
@@ -9,15 +8,19 @@ export class DuckFeedService {
 
     constructor(private duckFeedRepository: DuckFeedRepository) {}
 
-    async addDuckFeed(duckFeedMoel: DuckFeedMoel): Promise<void> {
+    async addDuckFeed(duckFeedMoel: DuckFeedMoel): Promise<DuckFeedMoel> {
         Logger.log('Start To Create Duck Feed', JSON.stringify(duckFeedMoel));
+
         const createdDuckFeed = await this.duckFeedRepository.create(duckFeedMoel);
+
         Logger.log('Success Create Duck Feed', JSON.stringify(createdDuckFeed));
+        return createdDuckFeed;
     }
 
     @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
     async addScheduledDuckFeed() {
         Logger.log('Start Job to add all scheduled feeds');
+
         let duckFeedItems = [];
         const limit = 50;
         let offset = 0;
@@ -25,10 +28,12 @@ export class DuckFeedService {
         do {
             duckFeedItems = await this.duckFeedRepository.findScheduledFeeds(limit, offset);
             Logger.log(`get feed patch ${patchNumber}`, JSON.stringify(duckFeedItems));
+
             if (duckFeedItems.length) {
                 const insertedDuckFeedItems = await this.duckFeedRepository.insertMany(duckFeedItems);
                 Logger.log(`added feed patch ${patchNumber}`, JSON.stringify(insertedDuckFeedItems));
             }
+
             offset += limit;
             patchNumber++;
         } while (duckFeedItems.length);
